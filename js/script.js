53718840b7457abcfe7226868d0bdb3afdfc108a
Noah <noahkoleske@icloud.com>
2023-06-18 18:11:15 -0500

    added comments in js file

--- js/script.js
+++ js/script.js
@@ -7,70 +7,68 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var numbers = ["1","2","3","4","5","6","7","8","9"]
 
 var specialChars = [" ", "!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@", "[" , "]", "^","_","`","{","|","}","~"]
+
+
+
 // Assignment Code
-
-
-
 function generatePassword() {
   
   var desiredChars = [upperCase, lowerCase, numbers, specialChars]
   pass = []
-  
-
   var charLength = prompt("Pick password length \n(8-128 characters)")
   charLength = parseInt(charLength)
-  
- 
+
   if (charLength < 8 || charLength > 128) {
     alert("Password must be between 8 and 128 characters long.")  
   } 
-  
-console.log(charLength)
+// Collects the user's password parameters
+  while (charLength >= 8 && charLength <= 128) {
+    upperCase = confirm("Do you want to use uppercase characters?")
+    if (upperCase) {
+      pass += desiredChars[0]
+    } 
 
-  upperCase = confirm("Do you want to use uppercase characters?")
-  if (upperCase) {
-    pass += desiredChars[0]
-  } 
+    lowerCase = confirm("Do you want to use lowercase characters?")
+    if (lowerCase) {
+      pass += desiredChars[1]
+    } 
+    
+    numbers = confirm("Do you want to use number characters?")
+    if (numbers) {
+      pass += desiredChars[2]
+    } 
+    
+    specialChars = confirm("Do you want to use special characters?")
+    if (specialChars) {
+      pass += desiredChars[3]
+    } 
 
-  lowerCase = confirm("Do you want to use lowercase characters?")
-  if (lowerCase) {
-    pass += desiredChars[1]
-  } 
-  
-  numbers = confirm("Do you want to use number characters?")
-  if (numbers) {
-    pass += desiredChars[2]
-  } 
-  
-  specialChars = confirm("Do you want to use special characters?")
-  if (specialChars) {
-    pass += desiredChars[3]
-  } 
-  
-  
-  for (var i = 0; i < pass.length; i++) {
-    pass = pass.replace(",", "")
-  }
-  
+    // Removes the commas from the list of characters selected
+    for (var i = 0; i < pass.length; i++) {
+      pass = pass.replace(",", "")
+    }
+    
 
-var answer = []
+  //  Code randomizes the characters the user chooses
+    var answer = []
 
-for (var i = 0; i < charLength; i++) {
-  var randomizeInput = pass[Math.floor(Math.random() * pass.length)]
-  answer.push(randomizeInput)
+    for (var i = 0; i < charLength; i++) {
+    var randomizeInput = pass[Math.floor(Math.random() * pass.length)]
+    answer.push(randomizeInput)
+    }
+
+    var displayPass = answer.join("")
+
+    return displayPass
+  } 
 }
 
-var displayPass = answer.join("")
-
-return displayPass
-
-}
 
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
